7ea9f501c69e5aa5f1c90c1c5727998248726293
mike <mikethecodegeek@gmail.com>
2021-07-21 14:45:04 -0700

    scoreboard and rules button created

---
+++ src/Components/Scoreboard/Score.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Score() {
+    return (
+        <div className="score">
+            <p>SCORE</p>
+            <p className="score-count">12</p>
+        </div>
+    )
+}
---
+++ src/Components/Scoreboard/Scoreboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Score from './Score'
+
+export default function Scoreboard() {
+    return (
+        <div className="scoreboard">
+            <h1>ROCK PAPER SCISSORS</h1>
+            <Score />
+        </div>
+    )
+}
